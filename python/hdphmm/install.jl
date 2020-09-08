be510bf51b61a56a11200947ef0d0cc002ed6b70
Maxime Mouchet <max@maxmouchet.com>
2020-09-08 14:03:06 +0200

    python: update tests

--- python/hdphmm/install.jl
+++ python/hdphmm/install.jl
@@ -1,7 +1,12 @@
 using Pkg
 
-pkg"""
-registry add General
-registry add git@github.com:SmartMonitoringSchemes/Registry.git
-add HDPHMM PyCall
-"""
+Pkg.Registry.add("General")
+Pkg.Registry.add(RegistrySpec(url="git@github.com:SmartMonitoringSchemes/Registry.git"))
+
+if haskey(ENV, "GITHUB_ACTIONS")
+    Pkg.add("PyCall")
+    Pkg.develop(path = joinpath(@__DIR__, "..", ".."))
+else
+    Pkg.add("PyCall")
+    Pkg.add("HDPHMM")
+end
--- python/tests/test_hdphmm.py
+++ python/tests/test_hdphmm.py
@@ -1,4 +1,24 @@
-from hdphmm.py import Beta, Gamma, TransitionDistributionPrior
+import random
+
+from hdphmm.py import (
+    Beta,
+    BlockedSamplerPrior,
+    DPGMMObservationModelPrior,
+    Gamma,
+    Normal,
+    NormalInverseChisq,
+    TransitionDistributionPrior,
+    robuststats,
+    segment,
+)
+
 
 def test_basic():
-    tp = None
+    data = [random.random() for _ in range(1000)]
+    obs_med, obs_var = robuststats(Normal, data)
+    tp = TransitionDistributionPrior(Gamma(2, 10), Gamma(100, 10), Beta(500, 1))
+    op = DPGMMObservationModelPrior(
+        NormalInverseChisq(obs_med, obs_var, 1, 10), Gamma(1, 0.5)
+    )
+    prior = BlockedSamplerPrior(1.0, tp, op)
+    segment(data, prior, L=10, LP=5, iter=10, verbose=True)
